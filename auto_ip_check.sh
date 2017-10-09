2278d9399c52092ce6a9faa67b999e6c6a47999f
CJ <4n0m4l0u5@users.noreply.github.com>
2017-10-09 09:10:01 -0600

    Added path to compiler, curl and better descriptions.

--- auto_ip_check.sh
+++ auto_ip_check.sh
@@ -1,6 +1,7 @@
-# IP checker and mailer
-#it does what it says, if you are like me and you are too cheap to buy a static IP address from your ISP but you want to know whenever your WAN IP changes, you can use this on a linux box that is powered all the time! Take that Comcast!
-#written by 4n0m4l0u5 in linux mint 17.3
+#!/bin/sh
+#Dynamic IP checker and emailer
+#it does what it says, if you are like me and you are too cheap to buy a static IP address from your ISP but you want to know whenever your WAN IP changes, you can use this! Take that Comcast!
+#written by 4n0m4l0u5 in ubuntu 16.04
 
 
 #create a new folder where you want to store this script, you will need to remember where you put this... for example /home/USERNAME/Documents/IP_Check
@@ -10,7 +11,7 @@
 #********SETUP********
 
 #enter your email address here inside the quotes:
-E_ADDR="xxxxxx@gmail.com"
+E_ADDR="youremail@gmail.com"
 
 #this is the variable that contains the path to the ip.txt file, change /home/USERNAME/Documents/IP_Check/ip.txt to the actual path, where ever you decided to put it...
 FILE="/home/USERNAME/Documents/IP_Check/ip.txt"
@@ -21,10 +22,11 @@ FILE="/home/USERNAME/Documents/IP_Check/ip.txt"
 #to set up postfix (etc/postfix/main.cf and etc/postfix/sasl_passwd) for gmail, go to https://rtcamp.com/tutorials/linux/ubuntu-postfix-gmail-smtp/ or you can use this installation summary:
 
 #install these packages:
-#sudo apt-get install postfix mailutils libsasl2-2 ca-certificates libsasl2-modules
+#sudo apt-get install postfix mailutils libsasl2-2 ca-certificates libsasl2-modules curl
 #choose "internet" (SMTP) mail service and use default FQDN
 
-#for gmail (or if you have something other than gmail you have to figure out what they use), add these lines to /etc/postfix/main.cf... also, if you have two step verification, you will have to generate an app password in gmail and use it here:
+#for gmail (or if you have something other than gmail you have to figure out what they use), add these lines to /etc/postfix/main.cf (remove hashtags)... eg. sudo nano /etc/postfix/main.cf also, if you have two step verification, you will have to generate an app password in gmail and use it in the next section (by the way, this is not very secure to keep your password in a file on your computer, even with no read permissions):
+
 #relayhost = [smtp.gmail.com]:587
 #smtp_sasl_auth_enable = yes
 #smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd
@@ -32,15 +34,17 @@ FILE="/home/USERNAME/Documents/IP_Check/ip.txt"
 #smtp_tls_CAfile = /etc/postfix/cacert.pem
 #smtp_use_tls = yes
 
-#open/create file: /etc/postfix/sasl_passwd
-#enter your info like so:[smtp.gmail.com]:587    USERNAME@gmail.com:PASSWORD
-#save and change permissions with this code:
+
+#open/create file: /etc/postfix/sasl_passwd	eg. sudo nano /etc/postfix/sasl_passwd
+#enter your info like so (no hashtag and use your username and app password):
+#[smtp.gmail.com]:587    USERNAME@gmail.com:PASSWORD
+#save and change file permissions with this code:
 #sudo chmod 400 /etc/postfix/sasl_passwd
 #sudo postmap /etc/postfix/sasl_passwd
 
 #validate certificates by running this code:
 #cat /etc/ssl/certs/Thawte_Premium_Server_CA.pem | sudo tee -a /etc/postfix/cacert.pem
-#you may need to look these certificates up and install them manually if your disro does not come with them pre installed (linux mint 18 for example)
+#you may need to look these certificates up and install them manually if your disro does not come with them pre installed (linux mint 18 or ubuntu 16.04 for example)
 
 #then reload postfix:
 #sudo /etc/init.d/postfix reload
@@ -50,6 +54,10 @@ FILE="/home/USERNAME/Documents/IP_Check/ip.txt"
 #append this line to run once every hour:
 #0 * * * * /home/USERNAME/Documents/IP_Check/auto_ip_check.sh
 #again, be sure to use the actual path you chose above and/or replace USERNAME with your actual...
+#make this file executable
+#run this code to test it...
+#./auto_ip_check.sh
+#it should email you immediately, or give you some errors if something isnt configured correctly, feel free to troubleshoot or send me an message on github
 
 
 #IP checker and mailer...
@@ -62,7 +70,7 @@ if [ -f "$FILE" ]   #check to see if the ip.txt file exists...
 
 then    #if it does exist, check the old contents with the newly returned IP address to see if they are different
 	IP_ADD_OLD=$(cat "$FILE")       #read contents of ip.txt into a variable
-	if [ $IP_ADD_OLD != $IP_ADD_NEW ]       #check and see if the IP address changed
+	if [ "$IP_ADD_OLD" != "$IP_ADD_NEW" ]       #check and see if the IP address changed
 	then
 		echo "$IP_ADD_NEW" | mail -s "IP Address change" $E_ADDR    #if it changed, mail out the new one
 		echo "$IP_ADD_NEW" > $FILE  #save new IP address to file
